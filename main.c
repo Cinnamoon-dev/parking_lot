533f1583ba2f0838e98e914c47ef4ab8bf2e8b1b
home <pedrolcsilva10@gmail.com>
2022-04-09 18:20:45 -0300

    variables set

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+const int TAM = 10;
+
+// Vars para servidores
+int codes_servs[TAM][10]; //obrigatório
+char nomes[TAM][255]; //obrigatório
+char SIAPE[TAM][10]; //obrigatorio
+int RGs[TAM][11];
+int CPFs[TAM][11]; //obrigatorio
+char enderecos[TAM][255];
+float salarios[TAM][10];
+int data_nasc[TAM][8]; //obrigatorio
+char tipos[TAM][30];
+
+//Vars para veículos
+int codes_cars[TAM][20]; //obrigatorio
+codes_servs[]; //de novo
+char desc[TAM][255]; //obrigatorio
+char placa[TAM][8]; //obrigatorio
+char marca[TAM][12]; //obrigatorio
+char modelos[TAM][20]; //obrigatorio
+
+char ocupados[TAM];
