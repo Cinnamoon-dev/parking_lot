88a2256c7cc37d52730b7cc43d6043f535dae00e
home <pedrolcsilva10@gmail.com>
2022-04-14 23:11:49 -0300

    insert servers funcionando perfeitamente Porém a comparação de strings para excluir com CPF n funciona com 11 algarismos mas funciona com menos (????)

--- main.c
+++ main.c
@@ -3,14 +3,14 @@
 #define TAM 10
 
 // Vars para servidores
-int codes_servs[TAM]; // campo obrigatório
+char codes_servs[TAM][10]; // campo obrigatório
 char nomes[TAM][255]; // campo obrigatório
 char SIAPEs[TAM][20]; // nao pode repetir
 char RGs[TAM][11]; // obrigatorio
-char CPFs[TAM][20]; // não pode repetir
+char CPFs[TAM][14]; // não pode repetir
 char enderecos[TAM][255]; // nao pode repetir
-char salarios[TAM][10]; // nao pode repetir
-char datas[TAM][10]; //obrigatorio
+char salarios[TAM][15]; // nao pode repetir
+char datas[TAM][11]; //obrigatorio
 char tipos[TAM][20]; // obrigatorio
 char ocupados[TAM]; // Diz qual espaço da lista está vazio
 
@@ -29,21 +29,27 @@ void iniciar() // Diz que todos os lugares na lista estao desocupados
     }
 }
 
-void exclude_serv(char nome[]) // Exclui os servidores por nome
+void exclude_serv() // Exclui os servidores por CPF
 {
+    char CPF[14];
+    printf("Digite o CPF do servidor:");
+    fgets(CPF, 13, stdin);
+    fflush(stdin);
     int index = -1;
     for (int i = 0; i < TAM; i++)
     {
-        if (strcmp(nomes[i], nome) == 0)
+        if (strcmp(CPFs[i], CPF) == 0)
         {
             index = i;
             ocupados[index] = 0;
             break;
+
         }
     }
+
 }
 
-void insert_serv(int codigo, char nome[], char SIAPE[], char CPF[], char RG[], char endereco[], char salario[], char data[], char tipo[])
+void insert_serv()
 {
     int index = -1;
     for (int i = 0; i < TAM; ++i)
@@ -54,7 +60,46 @@ void insert_serv(int codigo, char nome[], char SIAPE[], char CPF[], char RG[], c
         }
     }
 
-    codes_servs[index] = codigo;
+    char codigo[10];
+    char nome[255];
+    char SIAPE[20];
+    char CPF[13];
+    char RG[13];
+    char endereco[255];
+    char salario[10];
+    char data[11];
+    char tipo[25];
+
+    printf("codigo:");
+    fgets(codigo, 10, stdin);
+    fflush(stdin);
+    printf("nome:");
+    fgets(nome, 255, stdin);
+    fflush(stdin);
+    printf("SIAPE:");
+    fgets(SIAPE, 20, stdin);
+    fflush(stdin);
+    printf("CPF:");
+    fgets(CPF, 12, stdin);
+    fflush(stdin);
+    printf("RG:");
+    fgets(RG, 12, stdin);
+    fflush(stdin);
+    printf("endereco:");
+    fgets(endereco, 255, stdin);
+    fflush(stdin);
+    printf("salario:");
+    fgets(salario, 15, stdin);
+    fflush(stdin);
+    printf("data de nascimento:");
+    fgets(data, 11, stdin);
+    fflush(stdin);
+    printf("tipo:");
+    fgets(tipo, 20, stdin);
+    fflush(stdin);
+    printf("\n");
+
+    strcpy(codes_servs[index], codigo);
     strcpy(nomes[index], nome);
     strcpy(SIAPEs[index], SIAPE);
     strcpy(CPFs[index] , CPF);
@@ -73,7 +118,7 @@ void list()
     for (int i = 0; i < TAM; ++i)
     {
         if(ocupados[i] == 1 ){
-            printf("%d\t%s\t %s\t%s\t%s\t%s\t%s\t%s\t%s\n",codes_servs[i], nomes[i], SIAPEs[i], RGs[i], CPFs[i], enderecos[i], salarios[i], datas[i], tipos[i]);
+            printf("%s#\t%s#\t %s#\t%s#\t%s#\t%s#\t%s#\t%s#\t%s#\n",codes_servs[i], nomes[i], SIAPEs[i], RGs[i], CPFs[i], enderecos[i], salarios[i], datas[i], tipos[i]);
         }
     }
     printf("##################################\n\n");
@@ -84,13 +129,15 @@ int main()
 {
     iniciar();
 
-    insert_serv(1,"Pedro", "1000", "60619967130", "70013003622", "Av. Paulista, 143", "R$1000,00", "03/05/2003", "docente");
+    insert_serv();
 
-    insert_serv(2, "Victor", "1001", "01253287699", "4312674500", "Rua do Congo, 12", "R$2000,00", "14/02/2000", "tec. adm.");
+    //insert_serv(1,"Pedro", "1000", "60619967130", "70013003622", "Av. Paulista, 143", "R$1000,00", "03/05/2003", "docente");
+
+    //insert_serv(2, "Victor", "1001", "01253287699", "4312674500", "Rua do Congo, 12", "R$2000,00", "14/02/2000", "tec. adm.");
 
     list();
 
-    exclude_serv("Pedro");
+    exclude_serv();
 
     list();
 
